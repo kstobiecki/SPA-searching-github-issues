94e48985f6af4106ce622966585d89394fd592ee
kamil stobiecki <kamil.stobiecki@neoteric.eu>
2020-01-13 20:21:26 +0100

    feat(): add unsubscribe to subscription

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,17 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {debounceTime, distinctUntilChanged, filter, map, switchMap} from 'rxjs/operators';
+import {catchError, debounceTime, distinctUntilChanged, filter, map, switchMap} from 'rxjs/operators';
 import {ApiService, SharedService} from '../../services';
 import {IssueInterface} from '../../interfaces';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.scss'],
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   public searchIssueControl: FormControl;
 
+  private subscription: Subscription = new Subscription();
   private debounceTime = 1000;
   private openIssuesQuery = 'is:open is:issue archived:false';
 
@@ -20,7 +22,7 @@ export class SearchBoxComponent implements OnInit {
 
   ngOnInit(): void {
     this.searchIssueControl = new FormControl('');
-    this.searchIssueControl.valueChanges
+    this.subscription.add(this.searchIssueControl.valueChanges
       .pipe(
         filter(val => val.length > 2),
         map(val => val.trim()),
@@ -30,10 +32,17 @@ export class SearchBoxComponent implements OnInit {
           const query = `${this.openIssuesQuery} ${q}`;
           this.sharedService.loadingData.next(true);
           return this.apiService.getOpenIssues(query);
+        }),
+        catchError(err => {
+          throw new Error('Error in source. Details: ' + err);
         })
       )
       .subscribe( (res: IssueInterface) => {
         this.apiService.issues.next(res);
-      });
+      }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
